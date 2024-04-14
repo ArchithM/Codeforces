f1f39f72fbbbaa264fe65227a2a33af480be304e
archi <archithmanduva@gmail.com>
2024-04-14 16:59:44 -0500

    Added a new Class called StonesOnTheTable

---
+++ StonesOnTheTable.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class StonesOnTheTable
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        int n = keyboard.nextInt();
+        String s = keyboard.nextLine();
+
+        int count = 0;
+        for (int i = 0; i < s.length() - 1; i++)
+        {
+            if (s.charAt(i) == s.charAt(i + 1));
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
