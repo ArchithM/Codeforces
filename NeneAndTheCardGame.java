ff765ee18cc9a90e861f55f8eefefa547ec6906a
archi <archithmanduva@gmail.com>
2024-04-21 09:54:33 -0500

    Edited yogurtSale

---
+++ NeneAndTheCardGame.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class NeneAndTheCardGame
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int i = 0; i < t; i++)
+        {
+            
+        }
+    }
+}
